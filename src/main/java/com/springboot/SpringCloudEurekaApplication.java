644730ebeca87cb3ba1cdbb64efeadc1dc40df39
shayne1989 <lowby@163.com>
2018-09-25 17:32:14 +0800

    eureka-server

---
+++ src/main/java/com/springboot/SpringCloudEurekaApplication.java
@@ -0,0 +1,14 @@
+package com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer//注解启动一个服务注册中心提供给其他应用进行对话
+public class SpringCloudEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudEurekaApplication.class, args);
+	}
+}
